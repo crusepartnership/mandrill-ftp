a8f2512744e477bbab6fb5cdf2ced7e871fdc807
Daniel Pigott <daniel@cruse.com.au>
2015-12-22 11:39:48 +1100

    Update to get uploading working

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 var express = require('express');
 var path = require('path');
+var util = require('util');
 var bodyParser = require('body-parser');
+var Logger = require(__dirname + '/lib/logger');
+var Uploader = require(__dirname + '/lib/uploader');
 var _ = require('lodash');
 var app = express();
 var router = express.Router();
-var ftp = require('ftp');
 var config = require(__dirname + '/config/config.json');
+var log = new Logger(config);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 router.post('/', function (req, res) {
-    var messages = JSON.parse(req.param('mandrill_events'));
-    console.log(messages);
+    var messages = JSON.parse(req.body.mandrill_events);
     _.each(messages, function (message) {
-        console.log(message.msg.text);
+        var msg = message.msg;
+        var rules = _.find(config.rules, 'subject', msg.subject);
+        var renameFilename = false;
+        if (rules !== undefined) {
+            renameFilename = rules.filename;
+        }
+        var route = _.find(config.routes, 'email', msg.email);
+        if (route !== undefined) {
+            var uploader = new Uploader(route.destination, log);
+            log.info(util.format('Found route route email %s', route.email));
+            if (msg.attachments !== undefined) {
+                _.each(msg.attachments, function (attachment) {
+                    uploader.upload(attachment.name, attachment.content);
+                });
+            } else {
+                log.error(util.format("No attachments found on email to route %s", route.email));
+            }
+        }
+        log.info(msg.text);
     });
+
     return res.status(200).send({message: 'OK'});
 });
 
